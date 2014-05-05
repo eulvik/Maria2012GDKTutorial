b1ce7239f4887ec2403676dadf565097d5a62326
Eirik Ulvik <eirik.ulvik@gmail.com>
2014-05-05 09:59:51 +0200

    First version of solution, track exercise

--- MapApplication/TrackViewModel.cs
+++ MapApplication/TrackViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
+using TPG.GeoFramework.Contracts;
+using TPG.GeoFramework.TrackCore.Contracts;
+using TPG.GeoFramework.TrackLayer.Contracts.Selection;
 using TPG.Maria.Common;
 using TPG.Maria.Contracts;
 using TPG.Maria.TrackContracts;
@@ -8,9 +12,11 @@ namespace MapApplication
     public class TrackViewModel
     {
         private readonly IMariaTrackLayer _mariaTracksLayer;
+        public ObservableCollection<ITrackData> SelectedTracks { get; private set; } 
 
         public TrackViewModel(IMariaTrackLayer mariaTracksLayer)
         {
+            SelectedTracks = new ObservableCollection<ITrackData>();
             _mariaTracksLayer = mariaTracksLayer;
             _mariaTracksLayer.LayerInitialized += OnLayerInitialized;
         }
@@ -27,6 +33,29 @@ namespace MapApplication
             
             //Finally we tell which track list to display tracks from.
             _mariaTracksLayer.ActiveTrackList = _mariaTracksLayer.TrackLists[0];
+
+            //Handle the track selection changed event in order to show information about currently selected track.
+            _mariaTracksLayer.ExtendedTrackLayer.TrackSelectionChanged += OnTrackSelectionChanged;
+        }
+
+        private void OnTrackSelectionChanged(object sender, TrackSelectionChangedEventArgs args)
+        {
+            //Remove tracks that has been deselected.
+            var deselectedTracksToRemove = SelectedTracks.Where(st => args.DeselectedTracks.Any(dt => st.TrackItemId.InstanceId == dt.InstanceId)).ToList();
+            foreach (ITrackData trackDataToRemove in deselectedTracksToRemove)
+            {
+                SelectedTracks.Remove(trackDataToRemove);
+            }
+
+            //Extract the track data objects using selected id.
+            string[] selectedIds = args.SelectedTracks.Select(x => x.InstanceId).ToArray();
+            ITrackData[] selectedTrackDatas = _mariaTracksLayer.GetTrackData(selectedIds);
+
+            //Push new selected tracks to our list of selected tracks.
+            foreach (ITrackData selectedTrackData in selectedTrackDatas)
+            {
+                SelectedTracks.Add(selectedTrackData);
+            }
         }
     }
 }
