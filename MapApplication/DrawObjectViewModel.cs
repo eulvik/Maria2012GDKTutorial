46f181bf41ccdab9f69f6e2a1e0c22e49e3dbcc9
Eirik Ulvik <eirik.ulvik@gmail.com>
2014-05-05 13:17:54 +0200

    Solved draw object exercises

--- MapApplication/DrawObjectViewModel.cs
+++ MapApplication/DrawObjectViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows.Input;
+using TPG.DrawObjects.Contracts.DrawObjectState;
 using TPG.GeoFramework.Core;
 using TPG.Maria.DrawObjectContracts;
 
@@ -8,6 +10,7 @@ namespace MapApplication
     public class DrawObjectViewModel
     {
         private readonly IMariaDrawObjectLayer _mariaDrawObjectLayer;
+        private const string _dataDir = "data";
 
         public DrawObjectViewModel(IMariaDrawObjectLayer mariaDrawObjectLayer)
         {
@@ -17,6 +20,44 @@ namespace MapApplication
 
         private void OnLayerInitialized()
         {
+            _mariaDrawObjectLayer.ExtendedDrawObjectLayer.LayerChanged += OnLayerChanged;
+
+            //Get all stored draw objects from files in the data directory.
+            if (!Directory.Exists(_dataDir))
+                Directory.CreateDirectory(_dataDir);
+
+            var files = Directory.GetFiles(_dataDir);
+            foreach (var file in files)
+            {
+                if (Path.GetExtension(file).Contains("xml"))
+                {
+                    string xml = File.ReadAllText(file);
+                    _mariaDrawObjectLayer.UpdateStore(xml);
+                }
+            }
+        }
+
+        private void OnLayerChanged(object sender, DataStoreChangedEventArgs args)
+        {
+            if (args.DataStoreChangedAction == DataStoreChangeAction.Delete)
+            {
+                foreach (string affectedDrawObject in args.AffectedDrawObjects)
+                {
+                    string file = string.Format("{0}/{1}.xml", _dataDir, affectedDrawObject);
+                    if (File.Exists(file))
+                        File.Delete(file);
+                }
+            }
+            else
+            {
+                foreach (string affectedDrawObject in args.AffectedDrawObjects)
+                {
+                    string file = string.Format("{0}/{1}.xml", _dataDir, affectedDrawObject);
+                    if (File.Exists(file))
+                        File.Delete(file);
+                    File.WriteAllText(file, _mariaDrawObjectLayer.GetDrawObjectXMLFromStore(affectedDrawObject));
+                }
+            }
         }
 
         public ObservableCollection<ICreationWorkflow> Workflows
