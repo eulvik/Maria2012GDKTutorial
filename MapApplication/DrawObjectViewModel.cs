881e0971e9f1ff2e9fea8cb406057116d5899328
Eirik Ulvik <eirik.ulvik@gmail.com>
2014-05-05 12:17:54 +0200

    Missed view model

---
+++ MapApplication/DrawObjectViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TPG.GeoFramework.Core;
+using TPG.Maria.DrawObjectContracts;
+
+namespace MapApplication
+{
+    public class DrawObjectViewModel
+    {
+        private readonly IMariaDrawObjectLayer _mariaDrawObjectLayer;
+
+        public DrawObjectViewModel(IMariaDrawObjectLayer mariaDrawObjectLayer)
+        {
+            _mariaDrawObjectLayer = mariaDrawObjectLayer;
+            _mariaDrawObjectLayer.LayerInitialized += OnLayerInitialized;
+        }
+
+        private void OnLayerInitialized()
+        {
+        }
+
+        public ObservableCollection<ICreationWorkflow> Workflows
+        {
+            get { return _mariaDrawObjectLayer.GenericCreationWorkflows; }
+        }
+
+        private DelegateCommand _creationWorkflowActivatedCommand;
+        public ICommand CreationWorkflowActivatedCommand
+        {
+            get {
+                return _creationWorkflowActivatedCommand ??
+                       (_creationWorkflowActivatedCommand = new DelegateCommand(CreationWorkflowActivated));
+            }
+        }
+
+        private void CreationWorkflowActivated(object obj)
+        {
+            var creationWorkflow = obj as ICreationWorkflow;
+            if (creationWorkflow == null)
+                return;
+
+            _mariaDrawObjectLayer.ExtendedDrawObjectLayer.ActivateCreationWorkflow(creationWorkflow.ObjectTypeId);
+        }
+
+        public ICommand EditPointsCommand
+        {
+            get { return _mariaDrawObjectLayer.EditPointsCommand; }
+        }
+
+        public ICommand DeleteDrawObjectCommand
+        {
+            get { return _mariaDrawObjectLayer.DeleteDrawObjectCommand; }
+        }
+    }
+}
